6106b787a2ff9eadfe9bd979ec5c274b78883b15
dimeprog <dimejiawoyefa5@gmail.com>
2022-04-21 21:22:00 +0100

    productOverviewScreen displayed

---
+++ dushshop/lib/widget/product_item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem({
+    required this.id,
+    required this.imageUrl,
+    required this.title,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(imageUrl),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.favorite,
+          ),
+        ),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
