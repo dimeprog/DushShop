b612ed93c47c5d6b9feb562d913c360f6b8cc6cf
dimeprog <dimejiawoyefa5@gmail.com>
2022-05-03 13:17:02 +0100

    added firebase database, and loading data from backend

--- dushshop/lib/provider/product_provider.dart
+++ dushshop/lib/provider/product_provider.dart
@@ -5,68 +5,68 @@ import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
 
 class ProductProvider with ChangeNotifier {
-  final List<Product> _item = [
-    Product(
-      id: 'p1',
-      title: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
-      price: 29.99,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    ),
-    Product(
-      id: 'p2',
-      title: 'Trousers',
-      description: 'A nice pair of trousers.',
-      price: 59.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-    ),
-    Product(
-      id: 'p3',
-      title: 'Yellow Scarf',
-      description: 'Warm and cozy - exactly what you need for the winter.',
-      price: 19.99,
-      imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-    ),
-    Product(
-      id: 'p4',
-      title: 'A Pan',
-      description: 'Prepare any meal you want.',
-      price: 49.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-    ),
-    Product(
-      id: 'p5',
-      description: 'for food and making juice',
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/04/15/08/04/strawberry-1330459_960_720.jpg',
-      price: 15,
-      title: 'strawberry',
-    ),
-    Product(
-        id: 'p6',
-        description: 'for bathing',
-        imageUrl:
-            'https://cdn.pixabay.com/photo/2017/07/16/22/22/bath-oil-2510783_960_720.jpg',
-        price: 100,
-        title: 'bath-oil'),
-    Product(
-        id: 'p7',
-        description: 'for moving about and traveling ',
-        imageUrl:
-            'https://cdn.pixabay.com/photo/2017/08/24/18/25/car-2677839_960_720.jpg',
-        price: 5000,
-        title: 'car'),
-    Product(
-        id: 'p8',
-        description: 'used for lighting during photo shot by the photographier',
-        imageUrl:
-            'https://cdn.pixabay.com/photo/2021/07/14/17/23/photography-6466671_960_720.jpg',
-        price: 1000,
-        title: 'photography light')
+  List<Product> _item = [
+    // Product(
+    //   id: 'p1',
+    //   title: 'Red Shirt',
+    //   description: 'A red shirt - it is pretty red!',
+    //   price: 29.99,
+    //   imageUrl:
+    //       'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    // ),
+    // Product(
+    //   id: 'p2',
+    //   title: 'Trousers',
+    //   description: 'A nice pair of trousers.',
+    //   price: 59.99,
+    //   imageUrl:
+    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    // ),
+    // Product(
+    //   id: 'p3',
+    //   title: 'Yellow Scarf',
+    //   description: 'Warm and cozy - exactly what you need for the winter.',
+    //   price: 19.99,
+    //   imageUrl:
+    //       'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    // ),
+    // Product(
+    //   id: 'p4',
+    //   title: 'A Pan',
+    //   description: 'Prepare any meal you want.',
+    //   price: 49.99,
+    //   imageUrl:
+    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    // ),
+    // Product(
+    //   id: 'p5',
+    //   description: 'for food and making juice',
+    //   imageUrl:
+    //       'https://cdn.pixabay.com/photo/2016/04/15/08/04/strawberry-1330459_960_720.jpg',
+    //   price: 15,
+    //   title: 'strawberry',
+    // ),
+    // Product(
+    //     id: 'p6',
+    //     description: 'for bathing',
+    //     imageUrl:
+    //         'https://cdn.pixabay.com/photo/2017/07/16/22/22/bath-oil-2510783_960_720.jpg',
+    //     price: 100,
+    //     title: 'bath-oil'),
+    // Product(
+    //     id: 'p7',
+    //     description: 'for moving about and traveling ',
+    //     imageUrl:
+    //         'https://cdn.pixabay.com/photo/2017/08/24/18/25/car-2677839_960_720.jpg',
+    //     price: 5000,
+    //     title: 'car'),
+    // Product(
+    //     id: 'p8',
+    //     description: 'used for lighting during photo shot by the photographier',
+    //     imageUrl:
+    //         'https://cdn.pixabay.com/photo/2021/07/14/17/23/photography-6466671_960_720.jpg',
+    //     price: 1000,
+    //     title: 'photography light')
   ];
 
   List<Product> get item {
@@ -84,22 +84,44 @@ class ProductProvider with ChangeNotifier {
     return _item.where((prod) => prod.isFavourite).toList();
   }
 
-  Future<void> addItem(Product product) {
+  Future<void> fetchAndSetData() async {
+    var url = Uri.parse(
+        'https://dushshop-4eb1e-default-rtdb.firebaseio.com/products.json');
+    try {
+      final response = await http.get(url);
+      print(json.decode(response.body));
+      final extractedData = json.decode(response.body) as Map;
+      final List<Product> loadedProduct = [];
+      extractedData.forEach((prodId, productData) {
+        loadedProduct.add(Product(
+          id: prodId,
+          description: productData['description'],
+          title: productData['title'],
+          imageUrl: productData['imageUrl'],
+          price: productData['price'],
+          isFavourite: productData['isFavourite'],
+        ));
+      });
+      _item = loadedProduct;
+      notifyListeners();
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  Future<void> addItem(Product product) async {
     // const url = 'https://dushshop-4eb1e-default-rtdb.firebaseio.com/products/';
     var url = Uri.parse(
         'https://dushshop-4eb1e-default-rtdb.firebaseio.com/products.json');
-    return http
-        .post(url,
-            body: jsonEncode({
-              'title': product.title,
-              'description': product.description,
-              'price': product.price,
-              'isFavourite': product.isFavourite,
-              'imageUrl': product.imageUrl,
-            }))
-        .then((response) {
-      // ignore: avoid_print
-      print(json.decode(response.body));
+    try {
+      final response = await http.post(url,
+          body: jsonEncode({
+            'title': product.title,
+            'description': product.description,
+            'price': product.price,
+            'isFavourite': product.isFavourite,
+            'imageUrl': product.imageUrl,
+          }));
       final newProduct = Product(
         id: json.decode(response.body)['name'],
         description: product.description,
@@ -111,7 +133,10 @@ class ProductProvider with ChangeNotifier {
       print(newProduct.id);
       _item.add(newProduct);
       notifyListeners();
-    });
+    } catch (err) {
+      print(err);
+      throw err;
+    }
   }
 
   void updateItem(String id, Product newProduct) {
--- dushshop/lib/screens/edit_product_screen.dart
+++ dushshop/lib/screens/edit_product_screen.dart
@@ -83,7 +83,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
     }
   }
 
-  void _saveForm() {
+  Future<void> _saveForm() async {
     final isValid = _form.currentState!.validate();
     if (!isValid) {
       return;
@@ -105,14 +105,31 @@ class _EditProductScreenState extends State<EditProductScreen> {
       });
       Navigator.of(context).pop();
     } else {
-      Provider.of<ProductProvider>(context, listen: false)
-          .addItem(editProduct)
-          .then((_) {
+      try {
+        await Provider.of<ProductProvider>(context, listen: false)
+            .addItem(editProduct);
+      } catch (err) {
+        showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: const Text('An error occured'),
+            content: const Text('something went wrong!'),
+            actions: [
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('okay'),
+              )
+            ],
+          ),
+        );
+      } finally {
         setState(() {
           isLoading = false;
         });
         Navigator.of(context).pop();
-      });
+      }
     }
   }
 
